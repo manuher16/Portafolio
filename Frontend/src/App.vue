53ed6106a456a69c0e9fe78da5687b02c3b9e183
manuher16 <73207879+manuher16@users.noreply.github.com>
2022-03-05 09:49:22 -0600

    Add files via upload

---
+++ Frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+  <Nav/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Nav from './components/principal/nav.vue'
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+  components: {
+    Nav
+  },
+  created(){
+   
+    
+  }
+};
+</script>
+<style>
+  .nav{
+    margin-top:30px
+  }
+</style>
